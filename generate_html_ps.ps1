579ae14fe89e4a21f0f90ddfdec43fd8495a7a12
ninjastyle800 <112907501+ninjastyle800@users.noreply.github.com>
2026-01-31 16:22:20 -0500

    website fix

--- generate_html_ps.ps1
+++ generate_html_ps.ps1
@@ -181,7 +181,6 @@ $html = @"
             margin: 0;
             display: -webkit-box;
             -webkit-line-clamp: 2;
-            line-clamp: 2;
             -webkit-box-orient: vertical;
             overflow: hidden;
             text-overflow: ellipsis;
